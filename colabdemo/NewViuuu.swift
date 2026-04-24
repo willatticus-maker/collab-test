0b2547a00d487c2b4a8448e3f6fd7d2a4c8490a4
Kevin Berfirer <kberfirer@gmail.com>
2026-04-24 09:01:17 -0400

    NewViuuu added

---
+++ colabdemo/NewViuuu.swift
@@ -0,0 +1,18 @@
+//
+//  NewViuuu.swift
+//  colabdemo
+//
+//  Created by Kevin Berfirer on 4/24/26.
+//
+
+import SwiftUI
+
+struct NewViuuu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewViuuu()
+}
